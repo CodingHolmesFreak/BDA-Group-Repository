0fda5051edb245270d9e68e3674b8c79b839d76a
gooodsss <liamgestiada04@gmail.com>
2026-03-07 19:11:46 +0800

    Update lab2.py

--- lab2.py
+++ lab2.py
@@ -8,16 +8,16 @@ df.printSchema()
 
 # Strategy 1: Repartitioning by Publisher
 
-partition_publisher = df.repartition("Publisher")   
+partition_publisher = df.repartition("Publisher(s)")   
 
-publisher_summary = partition_publisher.filter(df["Publisher"]== "Tencent Games")
+publisher_summary = partition_publisher.filter(df["Publisher(s)"]== "Tencent Games")
 
 print("Summary of Tencent Games:")
 publisher_summary.show()
 
 #Transformations
 
-tencent_data = df.filter(df["Publisher"].contains("Tencent Games"))
+tencent_data = df.filter(df["Publisher(s)"].contains("Tencent Games"))
 
 print("Tencent Games:")
 tencent_data.select()
@@ -26,3 +26,5 @@ sorted_date = df.orderBy("Release date", ascending=False)
 
 print("Latest Games:")
 sorted_date.select("Game", "Release date", "Publisher").show(10)
+
+spark.stop()
