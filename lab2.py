092db6d0728cd41b6c295a7d6c902007810bbc65
gooodsss <liamgestiada04@gmail.com>
2026-03-07 19:47:48 +0800

    Update lab2.py

--- lab2.py
+++ lab2.py
@@ -2,7 +2,7 @@ from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName("Lab2").getOrCreate()
 
-df = spark.read.csv("most_played_mobile_games.csv", header=True, inferSchema=True)
+df = spark.read.csv("most_played_mobile_game.csv", header=True, inferSchema=True)
 
 df.printSchema()
 
