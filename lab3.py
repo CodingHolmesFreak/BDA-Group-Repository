f921ac3fbc7345410f9a14e3148028cdf03c943e
Raluiga <bjda2023-8399-13766@bicol-u.edu.ph>
2026-03-15 21:27:07 +0800

    Create lab3.py

---
+++ lab3.py
@@ -0,0 +1,44 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName("Lab3").getOrCreate()
+
+df = spark.read.csv("housing.csv", header=True, inferSchema=True)
+
+print("Original Dataset:")
+df.show(5)
+
+df_clean = df.dropna()
+
+selected_df = df_clean.select(
+    "median_income",
+    "median_house_value",
+    "ocean_proximity",
+    "population"
+)
+
+#Filter houses with income greater than 5
+filtered_df = selected_df.filter(selected_df.median_income > 5)
+
+print("Filtered Dataset:")
+filtered_df.show(5)
+
+#SQL temporary view
+df_clean.createOrReplaceTempView("housing_data")
+
+#top 5 most frequent ocean proximity categories
+result = spark.sql("""
+    SELECT ocean_proximity, COUNT(*) AS total_houses
+    FROM housing_data
+    GROUP BY ocean_proximity
+    ORDER BY total_houses DESC
+    LIMIT 5
+""")
+
+print("Top 5 Ocean Proximity Categories:")
+result.show()
+
+result.write.csv("overwiew").csv("output/top_proximity.csv", header=True)
+result.write.json("overview").json("output/top_proximity.json")
+result.write.mode("overwrite").format("text").save("output/top_proximity.txt")
+
+spark.stop()
